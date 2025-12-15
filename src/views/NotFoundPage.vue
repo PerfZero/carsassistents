e464760e3bb2ad418bbda430c0775e0be81d5c11
PerfZero <gundenis1@gmail.com>
2025-12-15 12:58:28 +0300

    fix

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen flex items-center justify-center p-4 bg-white">
     <div class=" text-center">
       <div class="mb-6">
-        <img src="/404.svg" alt="404" class="mx-auto mb-6" />
+        <h1 class="text-[#204A8A] font-gt-walsheim font-bold text-[136px] leading-[130%]">404</h1>
       </div>
       <p class="text-[#272827] font-normal text-lg">
         Некорректная ссылка. Обратитесь к менеджеру салона
